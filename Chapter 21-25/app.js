476df08f2ce47135dc3ceca41b2f37faa2e032fa
asifahmed786 <asifahmed1us1@gmail.com>
2020-06-16 18:34:01 +0500

    completed 21 to 25

--- Chapter 21-25/app.js
+++ Chapter 21-25/app.js
@@ -1,159 +1,201 @@
 // Task 1
-var arr = [[4,2,4],[3,6,7]];
-alert(arr[0][1])
+var fname = prompt("Please enter your first name");
+var lname = prompt ("Please enter your last name");
+var fullName = (fname+" "+lname)
+alert("Welcome to the site "+fullName)
 
 
 
 // Task 2
-var arr = [[0,1,2,3],[1,0,1,2],[2,1,0,1]];
+var mobile = prompt("Please enter your favourite mobile phone model")
+document.write("My favourite mobile phone is: "+mobile+"<br>");
+document.write("Length of string is: "+mobile.length)
+document.write("<br><br><br>")
 
 
 // Task 3
-for (var i = 0; i< 10; i++){
-    document.write((i+1)+"<br>")
-}
+var str = "Pakistani"
+document.write("String: "+str+"<br>");
+document.write("Index of 'n': "+str.indexOf("n"))
 document.write("<br><br><br>")
-
 
 
 // Task 4
-var tableNumber = +prompt("Enter a number to show its multipilication table")
-var tableLength = +prompt("Enter length multipilication table")
-for (var i = 0;i<tableLength;i++){
-    document.write(tableNumber+" X "+(i+1)+" = "+(tableNumber*tableLength)+"<br>")
-}
+var str1 = "Hello World"
+document.write("String: "+str1+"<br>");
+document.write("Last Index of 'l': "+str1.lastIndexOf("l"))
 document.write("<br><br><br>")
+
 
 
 
 // Task 5
-var fruits = ["apple","banana","mango","orange","strawberry"];
-
-for(var i = 0;i<fruits.length;i++){
-    document.write(fruits[i]+"<br>")
-}
-document.write("<br>")
-
-for(var i = 0;i<fruits.length;i++){
-    document.write("Element at index "+i+" is "+fruits[i]+"<br>")
-}
+var str = "Pakistani"
+document.write("String: "+str+"<br>");
+document.write("Character at index 3: "+str.charAt(3));
 document.write("<br><br><br>")
 
 
 
 // Task 6
-document.write("<h1>Counting</h1>")
-for(var i = 0; i<15;i++){
-    document.write(i+",")
-}
-document.write("<br>")
-
-document.write("<h1>Reverse Counting</h1>")
-for(var i = 10; i>0;i--){
-    document.write(i+",")
-}
-document.write("<br>")
-
-
-document.write("<h1>Even</h1>")
-for(var i = 0; i<=20;i++){
-    if((i%2) === 0){
-        document.write(i+",")
-    }
-    else{
-        continue
-    }    
-}
-document.write("<br>")
-
-
-
-document.write("<h1>Odd</h1>")
-for(var i = 0; i<=20;i++){
-    if((i%2) !== 0){
-        document.write(i+",")
-    }
-    else{
-        continue
-    }    
-}
-document.write("<br>")
-
-
-
-document.write("<h1>Even</h1>")
-for(var i = 0; i<=20;i++){
-    if(i===0){
-        continue
-    }
-    if((i%2) === 0){
-        document.write(i+"k,")
-    }
-    else{
-        continue
-    }    
-}
-document.write("<br>")
+var fname = prompt("Please enter your first name");
+var lname = prompt ("Please enter your last name");
+var fullName = fname.concat(" ",lname)
+alert("Welcome to the site "+fullName)
+document.write("<br><br><br>")
 
 
 
 // Task 7
-var bakery = ["cake","apple pie","cookie","chips","patties"]
-var item = prompt("Welcome to ABC bakery. What do you want to order sir/ma'am")
-for(var i = 0;i<bakery.length;i++){
-    if(item===bakery[i]){
-        alert("cookie is available at index "+i+" in our bakery")
-        break
-    }
-    else{
-        if((i+1)===bakery.length){
-            alert("We are sorry "+item+" is not available in our bakery")
-        }
-    }
-    
-}
+var city = "Hyderabad";
+document.write("City: "+city+"<br>")
+city = city.replace("Hyder","Islam")
+document.write("After replacement: "+city+"<br>")
 document.write("<br><br><br>")
-
-
-
 
 
 // Task 8
-var arrItem = [24,53,78,91,12]
-document.write("Array items: ")
-var largest = arrItem[0]
-for(var i = 0;i<arrItem.length;i++){
-    if(arrItem[i]>largest){
-        largest = arrItem[i]
-    }
-    document.write(arrItem[i]+",")
-}
-document.write("<br>")
-document.write("The largest number is: "+largest)
+var messages = "Ali and Sami are best friends. They play cricket and football together";
+document.write(messages.replace(/and/g,"&"))
 document.write("<br><br><br>")
 
 
 
-// Task 9
-var arrItem = [24,53,78,91,12]
-document.write("Array items: ")
-var small = arrItem[0]
-for(var i = 0;i<arrItem.length;i++){
-    if(arrItem[i]<small){
-        small = arrItem[i]
-    }
-    document.write(arrItem[i]+",")
-}
-document.write("<br>")
-document.write("The smallest number is: "+small)
+// task 9
+var numStr = "472";
+var num = parseInt(numStr)
+document.write("Value: "+numStr+"<br>")
+document.write("Type: "+typeof(numStr)+"<br>")
+document.write("Value: "+num+"<br>")
+document.write("Type: "+typeof(num)+"<br>")
 document.write("<br><br><br>")
-
 
 
 
 // Task 10
-for(var i = 0;i<=100;i++){
-    if(i>0 && (i%5)===0){
-        document.write(i+",")
+var x = prompt("Please enter something for converting to uppercase")
+document.write("User input: "+x+"<br>")
+document.write("Upper  case: "+x.toUpperCase()+"")
+document.write("<br><br><br>")
+
+
+
+// Task 11
+
+function title(string) {
+    var text1 = string.toLowerCase().split(" ");
+    for(var i = 0; i < text1.length; i++){
+       text1[i] = text1[i][0].toUpperCase() + text1[i].slice(1);
     }
+ return text1;
+ }
+var x = prompt("Please enter something for converting to title case")
+var titleString = new String(title(x));
+titleString = titleString.replace(/,/g," ");
+document.write("User input: "+x+"<br>")
+document.write("Title case: "+titleString+"")
+document.write("<br><br><br>")
+
+
+
+// Task 12
+var num = 35.36;
+var str1 = num.toString().replace(".","");
+document.write("Number: "+num+"<br>");
+document.write("Result: "+str1+"<br>");
+document.write("<br><br><br>")
+
+
+
+// Task 13
+var str1 = prompt("Please enter username");
+if(str1.indexOf("!")== -1 && str1.indexOf(",")==-1 && str1.indexOf(".")== -1 && str1.indexOf("@")== -1){
+   // alert("Please enter proper username")
 }
+else{
+   alert("Please enter a valid user name")
+}      
+
+
+
+
+// Task 14
+var items = ["cake","apple pie","cookie","chips","patties"]
+var userInput = prompt("Welcome to ABC Bakery. What do you want to order sir/ma'am?")
+for(var i = 0; i<items.length;i++){
+   if(userInput===items[i]){
+      alert("cookie is available at the index "+i+" in our bakery")
+      break;
+   }
+   else{
+      if(i+1 === items.length){
+         alert("we are sorry. "+userInput+" is not available in our bakery");
+      }
+      
+
+   }
+}
+
+
+
+
+// Task 15
+var condition = false;
+var pass = prompt("Pleaes enter passwor with following criteri:\nit should contain alphabets and numbers\nIt should not start with a number\nIt must at least 6 characters long")
+if(pass.length>=6){
+   alert("quantity is right "+pass.charCodeAt(pass[0]))
+   if(pass.charCodeAt(0) <= 57){
+      alert("Password cannot begin with a number")
+   }
+   else{
+         for(var i = 0;i <pass.length;i++){
+            if(pass.charCodeAt(i)<=57 && pass.charCodeAt(i)>=48){
+               contition = true;
+            }
+            
+         }  
+         if(condition===true){
+            alert("Password meets all criterias")
+         }
+         else{
+            alert("Password must contains numbers and characters")
+         } 
+
+   }
+}
+else{
+   alert("Please enter a valid password")
+}
+
+
+
+// Task 16
+var university = "University of Karachi"
+var uni = university.split("");
+for(var i = 0; i < uni.length; i++){
+   document.write(university[i]+"<br>")
+}
+document.write("<br><br><br>")
+
+
+
+
+// Task 17
+var x = prompt('Please enter word');
+document.write("User input: "+x+"<br>");
+document.write("Last character of input: "+x[x.length-1]+"<br>")
+document.write("<br><br><br>")
+
+
+
+// Task 18
+var text = "The quick brown fox jumps over the lazy dog";
+document.write("Text: "+text+"<br>");
+var no1 = 0;
+var arr1 = text.toLowerCase().split(" ");
+for(var i = 0;i < arr1.length;i++){
+   if(arr1[i] == "the"){
+      no1++;
+   }
+}
+document.write("There are "+no1+" occurence(s) of word 'the'")
